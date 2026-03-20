8d525f0891141ba4f2344b7ed1905d399c449522
Syed Imran <syedimranmurtaza32@gmail.com>
2026-03-20 08:11:24 +0500

    added

--- netlify/functions/review-removal-checkout.js
+++ netlify/functions/review-removal-checkout.js
@@ -4,14 +4,14 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = async (event) => {
 
-  // CORS headers
+
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Allow-Methods": "POST, OPTIONS"
   }
 
-  // Handle preflight request
+
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
